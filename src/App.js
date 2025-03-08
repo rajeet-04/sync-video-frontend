4266b066929e9f34c435e6359b140a12f81fe215
rajeet <rajeetash@hotmail.com>
2025-03-08 10:20:49 +0530

    update back_url

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useState, useEffect, useRef } from "react";
 import { io } from "socket.io-client";
 
-const socket = io("https://your-backend.up.railway.app"); // Replace with your Railway backend URL
+const socket = io("sync-video-backend-production.up.railway.app"); // Replace with your Railway backend URL
 
 export default function SyncStream() {
   const [gdriveLink, setGdriveLink] = useState("");
